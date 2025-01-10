c80d7b522e412525b91437d88cd8717a67bd3c7b
twtyygypl <35924310+twtyygypl@users.noreply.github.com>
2025-01-11 01:58:05 +0800

    更新 diy-part1.sh

--- config/immortalwrt-master/diy-part1.sh
+++ config/immortalwrt-master/diy-part1.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-#========================================================================================================================
-# https://github.com/ophub/amlogic-s9xxx-openwrt
-# Description: Automatically Build OpenWrt
-# Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
-# Source code repository: https://github.com/immortalwrt/immortalwrt / Branch: master
-#========================================================================================================================
 
-# Add a feed source
-# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# 修改默认主机名
+sed -i 's/ImmortalWrt/MyCustomHostname/g' package/base-files/files/bin/config_generate
 
-# other
-# rm -rf package/emortal/{autosamba,ipv6-helper}
+# 设置默认语言为简体中文
+sed -i 's/CONFIG_LANG="en"/CONFIG_LANG="zh_CN"/' .config
 
+# 开启某些额外的功能（例如 IPv6）
+sed -i 's/CONFIG_IPV6=n/CONFIG_IPV6=y/' .config
+
+# 选择默认的内核配置
+sed -i 's/CONFIG_TARGET_x86_64=y/CONFIG_TARGET_x86_64=y/' .config
+sed -i 's/CONFIG_TARGET_x86_64_DEFAULT=y/CONFIG_TARGET_x86_64_DEFAULT=y/' .config
+
+# 添加自定义软件包
+# 假设你已经准备好了额外的软件包目录
+# echo 'src-git custom_packages https://github.com/yourusername/custom-packages' >> feeds.conf.default
+# ./scripts/feeds update -a
+# ./scripts/feeds install -a
